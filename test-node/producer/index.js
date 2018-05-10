7dcb9dd5ebdfcc179a082f9c0c08142691e7c4c0
Sergio Martín <smartina@retailgas.biz>
2018-05-10 13:27:41 +0200

    Test 4 - Prueba de envio masivo de datos - for

--- test-node/producer/index.js
+++ test-node/producer/index.js
@@ -35,36 +35,35 @@ var values = 10;
 var valuesArr = [];
 var schemaObj;
 for(var x = 0; x<values; x++) {
+    
+    //En base a los valores del ifc se añadirian al esquema
     avroSchema.id = x+"";
     avroSchema.timestamp = Date.now();
-    valuesArr.push(avroSchema);
+    
+    producer.on('ready', function() {
+            var messageBuffer = type.toBuffer({
+                id: avroSchema.id,
+                timestamp: avroSchema.timestamp
+            });
+
+            var payload =  [{
+             topic: 'mongo-kafka',
+             messages: messageBuffer,
+             attributes: 1
+            }];
+
+            producer.send(payload, function(error, result){
+               if(error) {
+                   console.error(error);
+               } else {
+                   var formattedResult = result[0];
+                   console.log('result: ', result);
+               }
+            });     
+    });
 }
 
-producer.on('ready', function() {
-    for(var x = 0; x <= valuesArr.length; x++) {
-        var messageBuffer = type.toBuffer({
-            id: valuesArr[x].id,
-            timestamp: valuesArr[x].timestamp
-        });
 
-        var payload =  [{
-         topic: 'mongo-kafka',
-         messages: messageBuffer,
-         attributes: 1
-        }];
-
-        producer.send(payload, function(error, result){
-           if(error) {
-               console.error(error);
-           } else {
-               var formattedResult = result[0];
-               console.log('result: ', result);
-           }
-        }); 
-    }
-    
-
-});
 
 producer.on('error', function() {
     console.log("algo fallo");
